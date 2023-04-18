ddb00d6aa2a878e5e8e7532fa9fa466aaea80985
Vladimir <market.pump@gmail.com>
2023-04-18 02:27:19 +0200

    validation site

--- src/js/script.js
+++ src/js/script.js
@@ -45,6 +45,92 @@ $(document).ready(function(){
   toggleClass('.catalog-item__back');
   toggleClass('.catalog-item__link');
 
+      //Modal
+
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn(600);
+  });
+
+  $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #order, #thanks').fadeOut(600);
+  });
+  
+  $('.buttons_mini').each(function(i) {
+    $(this).on('click', function() {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn(600);
+    });
+  });
+
+function validateForm (form) {
+  $(form).validate( {
+    rules: {
+      name: {
+        required: true,
+        minlength: 2
+        // maxlength: 10
+      },
+      phone: 'required',
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      name:  {
+        required: "Пожалуйста, введите свое имя",
+        minlength: jQuery.validator.format("Необходимо ввести хотя бы {0} символа!")
+      },
+  
+      phone: "Пожалуйста, введите номер телефона",
+      email: {
+        required: "Почтовый адресс нужен для связи с Вами",
+        email: "Неправильный формат адресса почты"
+      }
+    }
+  });
+};
+
+validateForm ('#consultation form');
+validateForm ('#consultation-form');
+validateForm ('#order form');
+
+$('Input[name=phone]').inputmask('+380 (99) 999-99-99');
+
+$('form').submit(function(e) {
+  e.preventDefault();
+  $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+  }).done(function() {
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+
+      $('form').trigger('reset');
+  });
+  return false;
+});
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 1500) {
+    $(".go_up").fadeIn()
+  } else {
+    $(".go_up").fadeOut()
+    }
+});
+
+$("a[href^='#']").click(function(){
+  const _href = $(this).attr("href");
+  $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+  return false;
+});
+
+
+new WOW().init();
+
+
 });
 
 // const slider = tns({
